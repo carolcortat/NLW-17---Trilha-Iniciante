030283ddc3ddc903f6dd880a3a54edfba120cf62
Carol Cortat <carolscortat@hotmail.com>
2024-09-10 19:53:48 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -27,8 +27,18 @@ let meta = {
     }
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+console.log(metas[1].value)
+
+   
+// meta.value = "não é mais ler um livro"
+// meta.log(meta.value)
 
 
 //function //arrow function
