d3349c901d8ae7a8cde05671bbc62d5eaf670d9d
Carol Cortat <carolscortat@hotmail.com>
2024-09-11 19:27:23 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -58,9 +58,28 @@
 
 // start()
 
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
 
-const start = async() => {
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta"})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia")
+        return
+    }
+
+     metas.push(
+        { value: meta, checked: false}
+    )
+}
+
+const start = async () => {
 
    while(true){
     // let opcao = "sair"
@@ -87,7 +106,8 @@ const start = async() => {
 
     switch(opcao){
         case "cadastrar":
-            console.log("vamos cadrastar")
+            await cadastrarMeta()
+            console.log(metas)
             break
             case "listar":
                 console.log("vamos listar")
