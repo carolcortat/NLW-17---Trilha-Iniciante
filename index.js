43bfc4f9f827f82efdf47a2288fda337fb33968b
Carol Cortat <carolscortat@hotmail.com>
2024-09-13 20:16:15 -0300

    metas abertas

--- index.js
+++ index.js
@@ -122,8 +122,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+        // != diferente
+    })
+    if(abertas.length == 0) {
+        console.log('Não existem metas abertas!')
+        return
+    }
+
+    await select({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -149,6 +165,10 @@ const start = async () => {
                 name: "Metas realizadas",
                 value: "realizadas"   
             },
+             {
+                name: "Metas abertas",
+                value: "abertas"   
+            },
             {
                 name: "Sair",
                 value: "sair"
@@ -167,6 +187,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                     console.log("Até a próxima!")
